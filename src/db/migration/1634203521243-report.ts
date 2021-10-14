17969b021f18df2310dc3c9b2eec7e98784df909
Jacob Saur <jsaur@kiva.org>
2021-10-14 10:46:39 +0100

    add typeorm setup and report db schema
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

---
+++ src/db/migration/1634203521243-report.ts
@@ -0,0 +1,27 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class report1634203521243 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE IF NOT EXISTS report (
+                id SERIAL PRIMARY KEY,
+                create_time TIMESTAMPTZ,
+                auth_method VARCHAR,
+                verifier_id VARCHAR,
+                verifier_user_id VARCHAR,
+                request_id VARCHAR,
+                session_id VARCHAR,
+                result_code VARCHAR,
+                success BOOLEAN
+            );`
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE IF EXISTS report;
+        `);
+    }
+
+}
